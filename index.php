67d9cf35e8eab72454601ffacab878fcc59706d0
Ben Hansen <bahansen@c28-0117-06.ad.mtu.edu>
2019-11-08 13:33:18 -0500

    Updating all web files

---
+++ index.php
@@ -0,0 +1,21 @@
+
+
+<html>
+
+ <head>
+   <style>
+     .content {
+     max-width: 500px;
+     margin: auto;
+     }
+   </style>
+  <title>TSP/SPM Scheduler</title>
+ </head>
+ <body>
+<h1> TSP/SPM Scheduler </h1>
+  <a href="https://classdb.it.mtu.edu/cs3141/teamnoname/calendar.html">Student Scheduling Page</a>
+<br>
+<br>
+  <a href="https://classdb.it.mtu.edu/cs3141/teamnoname/admin.html">Professor Scheduling Page</a>
+ </body>
+</html>
